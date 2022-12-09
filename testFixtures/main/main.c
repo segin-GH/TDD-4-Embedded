f55eb0c1fc23f7ecbab04aadb1b2227a7b6b8398
segin-GH <segin.bytes@gmail.com>
2022-12-10 01:06:24 +0530

    .

--- testFixtures/main/main.c
+++ testFixtures/main/main.c
@@ -2,9 +2,53 @@
 #include <string.h>
 #include <unity.h>
 #include <unity_fixture.h>
+#include "unity_fixture_extras.h"
 
-void app_main()
+
+TEST_GROUP(TEST_1);
+
+static char output[100];
+static const char *expected;
+
+TEST_SETUP(TEST_1)
+{
+    memset(output,0xaa, sizeof output);
+    expected ="";
+}
+
+TEST_TEAR_DOWN(TEST_1)
 {
     //
 }
 
+TEST_GROUP_RUNNER(TEST_1)
+{
+    RUN_TEST_CASE(TEST_1,noFormatOperation);
+    RUN_TEST_CASE(TEST_1,insertString);
+}
+
+static void run_all_tests(void)
+{
+    RUN_TEST_GROUP(TEST_1);
+}
+
+void app_main(void)
+{
+    UNITY_MAIN_FUNC(run_all_tests);
+}
+
+
+TEST(TEST_1,noFormatOperation)
+{
+    char output[5] = "";
+    TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
+    TEST_ASSERT_EQUAL_STRING("hey", output);
+
+}
+
+TEST(TEST_1,insertString)
+{
+    char output[20] = "";
+    TEST_ASSERT_EQUAL(12, sprintf(output, "Hello %s\n", "world"));
+    TEST_ASSERT_EQUAL_STRING("Hello world\n", output);
+}
