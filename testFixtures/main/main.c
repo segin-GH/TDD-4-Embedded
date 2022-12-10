f7a77fc608546e76d7a48ddb3b3f2e5a67c24d35
segin-GH <segin.bytes@gmail.com>
2022-12-10 17:38:47 +0530

    comments

--- testFixtures/main/main.c
+++ testFixtures/main/main.c
@@ -3,46 +3,39 @@
 #include <unity.h>
 #include <unity_fixture.h>
 
+/**
+ *  @brief way to write test in test fixtures
+ */
 
-TEST_GROUP(TEST_1);
 
 static char output[100];
 static const char *expected;
 
+TEST_GROUP(TEST_1);
+
+/**
+ *  @brief similar to a constructor
+ */
 TEST_SETUP(TEST_1)
 {
     memset(output,0xaa, sizeof output);
     expected ="";
 }
 
-TEST_TEAR_DOWN(TEST_1)
-{
-    //
-}
+/**
+ *  @brief similar to a deconstructor 
+ */
+TEST_TEAR_DOWN(TEST_1){}
 
-TEST_GROUP_RUNNER(TEST_1)
-{
-    RUN_TEST_CASE(TEST_1,noFormatOperation);
-    RUN_TEST_CASE(TEST_1,insertString);
-}
-
-static void run_all_tests(void)
-{
-    RUN_TEST_GROUP(TEST_1);
-}
-
-void app_main(void)
-{
-    UNITY_MAIN_FUNC(run_all_tests);
-}
-
+/**
+ *  @brief tests for sprintf
+ */
 
 TEST(TEST_1,noFormatOperation)
 {
     char output[5] = "";
     TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
     TEST_ASSERT_EQUAL_STRING("hey", output);
-
 }
 
 TEST(TEST_1,insertString)
@@ -51,3 +44,29 @@ TEST(TEST_1,insertString)
     TEST_ASSERT_EQUAL(12, sprintf(output, "Hello %s\n", "world"));
     TEST_ASSERT_EQUAL_STRING("Hello world\n", output);
 }
+
+/**
+ *  @brief grups all similar tests
+ */
+TEST_GROUP_RUNNER(TEST_1)
+{
+    RUN_TEST_CASE(TEST_1,noFormatOperation);
+    RUN_TEST_CASE(TEST_1,insertString);
+}
+
+/**
+ *  @brief a func that runs all the tests
+ */
+static void run_all_tests(void)
+{
+    RUN_TEST_GROUP(TEST_1);
+}
+
+/** 
+ * @brief calling tests in main
+ */
+void app_main(void)
+{
+    UNITY_MAIN_FUNC(run_all_tests);
+}
+
