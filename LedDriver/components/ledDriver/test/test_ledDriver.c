7f62961f4db00b42f4d11577e67bde82c4a0b7cb
segin-GH <segin.bytes@gmail.com>
2022-12-14 02:03:00 +0530

    typo

--- LedDriver/components/ledDriver/test/test_ledDriver.c
+++ LedDriver/components/ledDriver/test/test_ledDriver.c
@@ -6,13 +6,13 @@
 
 /** @todo LED DRIVER TESTS 
  *
- *  //TODO are all leds off after init
+ *  //TODO are all led's off after init
  *  //TODO a single led can be turned on
  *  //TODO a single led can be turned off
- *  //TODO Multiple leds can be turned on/off
- *  //TODO Turn all leds on/off
+ *  //TODO Multiple led's can be turned on/off
+ *  //TODO Turn all led's on/off
  *  //TODO Query Led state
- *  //TODO check bouddary value
+ *  //TODO check boundary value
  *  //TODO check out-of-bound value 
  */
 
