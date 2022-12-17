8a841eaff5ff1040fcbea4acfa6099631b6961b7
segin-GH <segin.bytes@gmail.com>
2022-12-18 03:41:55 +0530

    added wrapper for update hardware, added comment

--- LedDriver/components/ledDriver/ledDriver.c
+++ LedDriver/components/ledDriver/ledDriver.c
@@ -1,7 +1,6 @@
 #include "ledDriver.h"
 
-const enum
-{
+const enum {
     ALL_LED_ON = ~0,
     ALL_LED_OFF = ~ALL_LED_ON
 }led_val;
@@ -9,12 +8,25 @@ const enum
 static uint16_t *virtualLedAddr;
 static uint16_t ledImg;
 
+
+/* wrapper function for updating hardware */
+void updateHardware()
+{
+    *virtualLedAddr = ledImg;
+}
+
+/* wrapper function for covert led number */
+static uint16_t convertLedNumToBit(int ledNumber)
+{
+    return 1 << (ledNumber - 1);
+}
+
 /* initialize led driver */
 void ledDriverInit(uint16_t *memoryMappedAddr)
 {
     virtualLedAddr = memoryMappedAddr;
     ledImg = ALL_LED_OFF;
-    *virtualLedAddr = ledImg;
+    updateHardware();
 }
 
 /* clean up led driver after no use */
@@ -23,28 +35,24 @@ void ledDriverDestroy(void)
     //
 }
 
-static uint16_t convertLedNumberTBit(int ledNumber)
-{
-    return 1 << (ledNumber - 1);
-}
 
 /* turn on one led */
 void ledDriverTurnON(int ledNumber)
 {
-    ledImg |= convertLedNumberTBit(ledNumber);
-    *virtualLedAddr = ledImg;
+    ledImg |= convertLedNumToBit(ledNumber);
+    updateHardware();
 }
 
 /* turn off one led */
 void ledDriverTurnOFF(int ledNumber)
 {
-   ledImg &= ~(convertLedNumberTBit(ledNumber));
-    *virtualLedAddr = ledImg; 
+    ledImg &= ~(convertLedNumToBit(ledNumber));
+    updateHardware(); 
 }
 
-
+/* turn all led on */
 void ledDriverTurnAllOn()
 {
-   ledImg = ALL_LED_ON;
-    *virtualLedAddr = ledImg; 
+    ledImg = ALL_LED_ON;
+    updateHardware(); 
 }
