23591f5730d736ca0093cf4821aa555c2ce2aad4
segin-GH <segin.bytes@gmail.com>
2022-12-17 17:30:02 +0530

    clean TODO list

--- LedDriver/components/ledDriver/test/test_ledDriver.c
+++ LedDriver/components/ledDriver/test/test_ledDriver.c
@@ -5,16 +5,19 @@
  */
 
 /** @todo LED DRIVER TESTS 
- *
- *  TODO are all led's off after init
- *  TODO a single led can be turned on
- *  //TODO a single led can be turned off
- *  //TODO Multiple led's can be turned on/off
- *  //TODO Turn all led's on/off
- *  //TODO Query Led state
- *  //TODO check boundary value
- *  //TODO check out-of-bound value 
- */
+
+    TODO a single led can be turned on
+    TODO Multiple led's can be turned on/off
+    TODO are all led's off after init
+    TODO a single led can be turned off
+    TODO Turn all led's on/off
+    TODO Query Led state
+    TODO check boundary value
+    TODO check out-of-bound value 
+*/
+
+
+
 
 #include <unity.h>
 #include <unity_fixture.h>
