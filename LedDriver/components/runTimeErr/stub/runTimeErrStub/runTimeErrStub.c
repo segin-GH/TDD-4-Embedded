3706de29cb545d7d7fb03d41a58f3ebc3716e83d
segin-GH <segin.bytes@gmail.com>
2022-12-24 02:33:25 +0530

    reverting stub
    This reverts commit 8f76d3b97cbefe2807bb1f0436fd474fc80354f8.

--- LedDriver/components/runTimeErr/stub/runTimeErrStub/runTimeErrStub.c
+++
@@ -1,6 +0,0 @@
-#include "runTimeErrStub.h"
-
-void runTimeStubWorking()
-{
-    printf("run time err stub working\n");
-}
--- LedDriver/components/runTimeErr/stub/runTimeErrStub/runTimeErrStub.h
+++
@@ -1,8 +0,0 @@
-#ifndef RUN_TIME_ERR_STUB_H
-#define RUN_TIME_ERR_STUB_H
-
-#include <stdio.h>
-
-void runTimeStubWorking();
-
-#endif
