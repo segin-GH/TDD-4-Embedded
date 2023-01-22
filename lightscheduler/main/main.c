77c04802ef2704001ea57dd149f21c823130658a
segin-GH <segin.bytes@gmail.com>
2023-01-22 09:39:32 +0530

    .

--- lightscheduler/main/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void app_main(void)
-{
-   printf("main.c got executed\n");
-}
--- wifiConnect/main/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <wifiConnect.h>
-#include <esp_log.h>
-
-
-void app_main(void)
-{
-    ESP_LOGW("[MAIN]", "Main got executed");
-}
