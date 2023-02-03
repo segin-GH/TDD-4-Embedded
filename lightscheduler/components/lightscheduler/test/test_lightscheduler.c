9f47d7f434725bacc7f10711b68cd90488d23401
segin-GH <segin.bytes@gmail.com>
2023-02-04 00:26:25 +0530

    .

--- lightscheduler/components/lightscheduler/test/test_lightscheduler.c
+++ lightscheduler/components/lightscheduler/test/test_lightscheduler.c
@@ -2,8 +2,27 @@
 #include <stdlib.h>
 #include <unity.h>
 
+int count = 0;
+
+void setUp(void)
+{
+    count = 10;
+}
+
+void tearDown(void)
+{
+    //
+}
 
 TEST_CASE("what_a_test","test_one")
 {
-    TEST_ASSERT_EQUAL(0,0)
+    TEST_ASSERT_EQUAL(10,count);
+    TEST_ASSERT_EQUAL_HEX16(0x1000,0x1000);
+    count++;
+}
+
+TEST_CASE("what_a_test_two","test_one")
+{
+    TEST_ASSERT_EQUAL(10,count);
+    TEST_ASSERT_EQUAL_HEX16(0x1000,0x1000);
 }
